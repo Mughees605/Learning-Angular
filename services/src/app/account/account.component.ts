6fb2fd0883f499c8608841ac684a379904b1864d
mughees605 <mughees605@gmail.com>
2017-06-24 05:30:51 +0500

    using heirarichal method for providers

--- services/src/app/account/account.component.ts
+++ services/src/app/account/account.component.ts
@@ -6,7 +6,7 @@ import { LoggingService } from '../logging.service'
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers:[AccountService, LoggingService]
+  providers:[LoggingService]
 })
 export class AccountComponent {
 
