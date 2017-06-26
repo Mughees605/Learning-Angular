6fc30b653b5e978b3dd7628115b6d2e2c8b9ded7
mughees605 <mughees605@gmail.com>
2017-06-27 02:22:33 +0500

    child routers 2

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -15,8 +15,11 @@ import { ServersService } from './servers/servers.service';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'user/:id/:name', component: UserComponent },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent },
+    ]
+  },
   {
     path: 'servers', component: ServersComponent, children: [
       { path: ':id/edit', component: EditServerComponent },
