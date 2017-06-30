40c8bd117096261a5b024dfff4ba9096c445e92d
mughees605 <mughees605@gmail.com>
2017-06-30 21:18:17 +0500

    adding in memory data service

--- angular-heroes/src/app/app.module.ts
+++ angular-heroes/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 import { HttpModule } from '@angular/http'
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api'
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './In-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -22,6 +23,7 @@ import { AppRoutingModule }     from './app-routing.module';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),    
     HttpModule
   ],
   providers: [
