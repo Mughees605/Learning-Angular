29a1bc2766ad6f1bd098883140ca65e28a673821
mughees605 <mughees605@gmail.com>
2017-07-04 01:07:34 +0500

    added angular fire and import module

--- angularAuthApp/src/app/app.module.ts
+++ angularAuthApp/src/app/app.module.ts
@@ -1,5 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http'
+import { FormsModule } from '@angular/forms'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database'
+import { AngularFireAuthModule } from 'angularfire2/auth'
+
+
+
+
+  // Initialize Firebase
+  export const firebaseConfig = {
+    apiKey: "AIzaSyAJUVixnpJEMoXuHzHfYNDTLk7-RDO0HY4",
+    authDomain: "ng-http-452a4.firebaseapp.com",
+    databaseURL: "https://ng-http-452a4.firebaseio.com",
+    projectId: "ng-http-452a4",
+    storageBucket: "ng-http-452a4.appspot.com",
+    messagingSenderId: "32230195720"
+  };
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +26,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    HttpModule,
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
