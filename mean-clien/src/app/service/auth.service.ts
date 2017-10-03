ffea4c60ab3e872cbaee24f9c075a5387640b31e
mughees605 <mughees605@gmail.com>
2017-10-03 17:02:48 +0500

    Complete login signup

--- mean-clien/src/app/service/auth.service.ts
+++ mean-clien/src/app/service/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/map';
-
+import { tokenNotExpired } from 'angular2-jwt';
 @Injectable()
 export class AuthService {
   authToken: any;
@@ -44,12 +44,7 @@ export class AuthService {
   }
 
   loggedIn() {
-   if(localStorage.getItem('id_token')){
-     return true
-   }
-   else{
-     return false
-   }
+   return tokenNotExpired('id_token')
   }
 
   url(ep) {
